33c5fe7b2251737ee5ad168313179e946323aed2
Vataz1337 <77028131+Vataz1337@users.noreply.github.com>
2022-10-11 23:12:03 +0200

    Lab 1 Done

---
+++ lab1/cmake-build-debug/CMakeFiles/lab1.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Desktop/Studia/Semestr 5/NAI/lab1/januszPrzyklad.cpp" "CMakeFiles/lab1.dir/januszPrzyklad.cpp.obj" "gcc" "CMakeFiles/lab1.dir/januszPrzyklad.cpp.obj.d"
+  "D:/Desktop/Studia/Semestr 5/NAI/lab1/main.cpp" "CMakeFiles/lab1.dir/main.cpp.obj" "gcc" "CMakeFiles/lab1.dir/main.cpp.obj.d"
+  "D:/Desktop/Studia/Semestr 5/NAI/lab1/przyklad.cpp" "CMakeFiles/lab1.dir/przyklad.cpp.obj" "gcc" "CMakeFiles/lab1.dir/przyklad.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
