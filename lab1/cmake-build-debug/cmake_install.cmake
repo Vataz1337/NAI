5fe80bc487ea9d42aa6066afa7757d315fa773cc
Vataz <patryksroka99@gmail.com>
2022-10-12 17:47:12 +0200

    lab done and checked

--- lab1/cmake-build-debug/cmake_install.cmake
+++ lab1/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/Desktop/Studia/Semestr 5/NAI/lab1
+# Install script for directory: C:/Users/s23205/Downloads/SztucznaInteligencja/NAI/lab1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "D:/Desktop/Studia/Semestr 4/SystemyOperacyjne/CLion 2021.3.3/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2022.2.1/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/Desktop/Studia/Semestr 5/NAI/lab1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/s23205/Downloads/SztucznaInteligencja/NAI/lab1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,32 +1,29 @@
 #include <iostream>
 #include <map>
 #include <functional>
+#include <utility>
 #include <vector>
 #include <string>
 #include <cmath>
 
 using namespace std;
-using myFunction = function<float(int x, int y)>;
+using myFunction = function<double(vector<double>)>;
 
-void printValues(const myFunction& func, int x, int y=0){
-    cout << func(x, y) << endl;
+void printValues(vector<double> numb, myFunction fun){
+    cout << fun(numb) << endl;
 }
 
 int main(int argc, char **argv) {
-    vector<string> list(argv, argc + argv);
     map<string, myFunction> options;
-    options["add"] = [](int x, int y) { return x + y; };
-    options["mod"] = [](int x, int y) { return x % y; };
-    options["sin"] = [](int x, int y=0) { return sin(x); };
+    options["add"] = [](vector<double> numbers) { return numbers.front() + numbers.back(); };
+    options["mod"] = [](vector<double> numbers) { return (int)numbers.front() % (int)numbers.back(); };
+    options["sin"] = [](vector<double>numbers) { return sin(numbers.front()); };
 try {
-    if (list.at(1)=="sin") {
-        printValues(options[list.at(1)], stoi(list.at(2)));
-    } else if (list.at(1)=="add" || list.at(1)=="mod") {
-        printValues(options[list.at(1)], stoi(list.at(2)), stoi(list.at(3)));
-    }else{
-        cout << "1.Wybierz jedna z trzech opcji: add, mod lub sin." << endl;
-    }
-}catch (out_of_range ofr){
+    vector<string> list(argv, argc + argv);
+    vector<double> numberes = {{stod(list.at(2)), stod(list.at(3))}};
+    printValues(numberes, options.at(list.at(1)));
+}
+catch (out_of_range ofr){
     cout << "2.Wybierz jedna z trzech opcji: add, mod lub sin." << endl;
 } catch (bad_function_call bfc) {
     cout << "3.Wybierz jedna z trzech opcji: add, mod lub sin." << endl;
