c323c747e44258d30444ce777a61e3b8c70049ce
Vataz1337 <77028131+Vataz1337@users.noreply.github.com>
2022-10-19 00:29:09 +0200

    Lab2 First Commit

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -9,20 +9,25 @@
 
 using namespace std;
 using myFunction = function<double(vector<double>)>;
-using dVector = vector<double>;
+using domainVector = vector<double>;
 
 random_device randomDevice;
 mt19937 mtGenerator(randomDevice());
 
-double bruteforce(const function<double(vector<double>)>& func, dVector domain){
+double bruteforce(const function<double(vector<double>)>& func, domainVector domain) {
     double lowestValue = numeric_limits<double>::max();
-    double lowestDomain = domain.at(0);
-    double highiestDomain = domain.at(1);
-    for(int i = 0 ; i < TESTING_PROBES ; i++){
-        uniform_real_distribution<double> randomValue(lowestDomain, highiestDomain);
-        vector<double> randomValuesOfxy{randomValue(mtGenerator),randomValue(mtGenerator)};
-        double temp = func(randomValuesOfxy);
-        lowestValue > temp ? lowestValue = temp : false;
+    double xLowestDomainValue = domain.at(0);
+    double xHighestDomainValue = domain.at(1);
+    double yLowestDomainValue = domain.at(2);
+    double yHighestDomainValue = domain.at(3);
+    for (int i = 0; i < TESTING_PROBES; i++) {
+        uniform_real_distribution<double> x(xLowestDomainValue, xHighestDomainValue);
+        uniform_real_distribution<double> y(yLowestDomainValue, yHighestDomainValue);
+        domainVector randomVlues = {x(mtGenerator), y(mtGenerator)};
+        double temp = func(randomVlues);
+        if (lowestValue > temp) {
+            lowestValue = temp;
+        }
     }
     return lowestValue;
 }
@@ -30,22 +35,22 @@ double bruteforce(const function<double(vector<double>)>& func, dVector domain){
 int main(int argc, char** argv){
     vector<string> inputValues(argv, argc + argv);
     map<string, myFunction> funcMap;
-    funcMap["BoothFunction"] = [](vector<double> xyValues){
-        if(xyValues.front()<-10 || xyValues.front()>10)
-            throw invalid_argument("Input x value must be higher of equal -10 and y value must be 10 or lower.");
-        return pow((xyValues.front()+2*(xyValues.back())-7),2)+pow((2*(xyValues.front())+xyValues.back()-5),2);};
-    funcMap["Himmelblau's function"] = [](vector<double> xyValues){
-        if(xyValues.front()<-5 || xyValues.front()>5)
-            throw invalid_argument("Input x value must be higher of equal -5 and y value must be 5 or lower.");
-        return pow((pow(xyValues.front(),2)+(xyValues.back())+11),2)+pow((xyValues.front()+pow(xyValues.back(),2)-7),2);};
-    funcMap["Matyas function"] = [](vector<double> xyValues){
-        if(xyValues.front()<-10 || xyValues.front()>10)
-            throw invalid_argument("Input x value must be higher of equal -10 and y value must be 10 or lower.");
-        return 0.26*(pow(xyValues.front(),2)+pow(xyValues.back(),2))-0.48*(xyValues.front()*xyValues.back());};
+    funcMap["BoothFunction"] = [](vector<double> values){
+        if(-10>values.front() || values.front()>10 || -10>values.back() || values.back()>10) throw invalid_argument("Input x value must be higher of equal -10 and y value must be 10 or lower.");
+        return pow((values.front() + 2 * (values.back()) - 7), 2) + pow((2 * (values.front()) + values.back() - 5), 2);};
+    funcMap["HimmelblauFunction"] = [](vector<double> values){
+        if(-5>values.front() || values.front()>5 || -5>values.back() || values.back()>5) throw invalid_argument("Input x value must be higher of equal -5 and y value must be 5 or lower.");
+        return pow((pow(values.front(), 2) + (values.back()) + 11), 2) + pow((values.front() + pow(values.back(), 2) - 7), 2);};
+    funcMap["MatyasFunction"] = [](vector<double> values){
+        if(-5>values.front() || values.front()>5 || -5>values.back() || values.back()>5) throw invalid_argument("Input x value must be higher of equal -10 and y value must be 10 or lower.");
+        return 0.26*(pow(values.front(), 2) + pow(values.back(), 2)) - 0.48 * (values.front() * values.back());};
     try{
-        cout << bruteforce(funcMap.at(inputValues.at(1)), {stod(inputValues.at(2)), stod(inputValues.at(3))});
+        cout << bruteforce(funcMap.at(inputValues.at(1)), {stod(inputValues.at(2)), stod(inputValues.at(3)), stod(inputValues.at(4)), stod(inputValues.at(5))});
     }catch(out_of_range e) {
-
+        cout << "Available functions:" << endl;
+        cout << "Matyas function(-10<=x,y<10)" << endl;
+        cout << "Himmelblau's function(-5<=x,y<5)" << endl;
+        cout << "Booth function(-10<=x,y<10)" << endl;
     }
     return 0;
 }
